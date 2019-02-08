8b356b7dced61388e8cb717c2bcd71a5fd61f782
nikola <ekv1sky@gmail.com>
2019-02-08 07:35:58 +0100

    Gitignore

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,14 @@
     <title>Laravel 5.7 CRUD Example Tutorial</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
-    <link rel="stylesheet" type="text/css" media="screen" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" media="screen"
+        href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/base/jquery-ui.css">
 </head>
+
 <body>
-<div class="container">
     @yield('content')
-</div>
-<script src="{{ asset('js/app.js') }}" type="text/js"></script>
+    </div>
+    <script src="{{ asset('js/app.js') }}" type="text/js"></script>
 </body>
+
 </html>
